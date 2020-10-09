45ef89ddcc66141a276c97cc5283429281f30d20
Waman Murudkar <waman.murudkar@gmail.com>
2020-10-09 21:01:33 +0530

    09/10/2020

---
+++ Waman/flattenList.cpp
@@ -0,0 +1,146 @@
+// { Driver Code Starts
+#include <bits/stdc++.h>
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+
+using namespace std;
+
+void printList(Node *Node)
+{
+    while (Node != NULL)
+    {
+        printf("%d ", Node->data);
+        Node = Node->bottom;
+    }
+}
+
+Node* flatten (Node* root);
+
+int main(void) {
+
+	int t;
+	cin>>t;
+	while(t--){
+    int n,m,flag=1,flag1=1;
+    struct Node * temp=NULL;
+	struct Node * head=NULL;
+	struct Node * pre=NULL;
+	struct Node * tempB=NULL;
+	struct Node * preB=NULL;
+		cin>>n;
+        int work[n];
+		for(int i=0;i<n;i++)
+            cin>>work[i];
+		for(int i=0;i<n;i++){
+			m=work[i];
+			--m;
+			int data;
+			scanf("%d",&data);
+			temp = new Node(data);
+			temp->next = NULL;
+			temp->bottom = NULL;
+			
+			if(flag){
+				head = temp;
+				pre = temp;
+				flag = 0;
+				flag1 = 1;
+			}
+			else{
+				pre->next = temp;
+				pre = temp;
+				flag1 = 1;
+			}
+			for(int j=0;j<m;j++){
+				
+				int temp_data;
+				scanf("%d",&temp_data);
+				tempB = new Node(temp_data);
+
+				if(flag1){
+					temp->bottom=tempB;
+					preB=tempB;
+					flag1=0;
+				}
+				else{
+					preB->bottom=tempB;
+					preB=tempB;
+				}
+			}
+		}
+		   Node *fun = head;
+		   Node *fun2=head;
+
+            Node* root = flatten(head);
+            printList(root);
+            cout<<endl;
+
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/* Node structure  used in the program
+
+struct Node{
+	int data;
+	struct Node * next;
+	struct Node * bottom;
+	
+	Node(int x){
+	    data = x;
+	    next = NULL;
+	    bottom = NULL;
+	}
+	
+};
+*/
+
+/*  Function which returns the  root of 
+    the flattened linked list. */
+    
+bool sortby(pair<int, Node*> &a, pair<int, Node*> &b) {
+    return a.first < b.first;
+}
+
+Node *flatten(Node *root)
+{
+   // Your code here
+   vector<pair<int, Node*>> vec;
+   Node* curr = root;
+   
+   while (curr) {
+       Node* tmp = curr;
+       vec.push_back({curr->data, curr});
+       while (tmp->bottom) {
+           tmp = tmp->bottom;
+           vec.push_back({tmp->data, tmp});
+       }
+       curr = curr->next;
+   }
+   
+   sort(vec.begin(), vec.end(), sortby);
+   
+   int n = vec.size();
+   
+   for (int i = 0; i < n - 1; i++) {
+       (vec[i].second)->bottom = (vec[i+1].second);
+   }
+   
+   (vec[n - 1].second)->bottom = NULL;
+   
+   return vec[0].second;
+}
+
